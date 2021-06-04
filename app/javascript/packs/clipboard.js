58385c7f79bbf8c906fa6ba14145cf2dd34afe77
andre <andre.domingo@sjsu.edu>
2021-06-04 13:57:03 -0700

    fixed copy to clipboard button

--- app/javascript/packs/clipboard.js
+++
@@ -1,6 +0,0 @@
-let copyToClipboard = function(){
-    let copyText = document.getElementById("textToCopy");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-    document.execCommand("copy");
-}
